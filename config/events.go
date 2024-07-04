1394ecf5734cfeb3055c7fd57fc2c3874e1ed14b
Alexis Bouchez <80361585+alexisbouchez@users.noreply.github.com>
2024-07-04 21:35:02 +0200

    refactor UI tabs to use UI component for consistency

--- config/events.go
+++ config/events.go
@@ -9,12 +9,10 @@ import (
 func RegisterEventsEmitter(
 	usersListener *listeners.UsersListener,
 	deploymentsListener *listeners.DeploymentsListener,
-	smtpListener *listeners.SMTPListener,
 ) *events.EventsEmitter {
 	emitter := events.NewEventsEmitter()
 	emitter.On("users.created", usersListener.OnCreated)
 	emitter.On("deployments.created", deploymentsListener.OnCreated)
-	emitter.On("smtp.outbound_email", smtpListener.OnOutboundEmail)
 
 	return emitter
 }
